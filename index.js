77cd06095f839c31606ace7d6bea15e9e62a2b86
Gadzhiev Islam <gadzhievislam@mac.local>
2025-01-21 15:09:49 +0300

    [TEST] Test cases outputs

--- index.js
+++ index.js
@@ -51,6 +51,14 @@ const iter = obj.iter(
   (...args) => console.log(args)
 )();
 
+// Outputs:
+// [ 'undefined', { a: 1, b: 2 } ]
+// [ 'key2', { c: 3, d: 4 } ]
+
 const newIter = obj.newIter(
   (...args) => console.log(args)
 )();
+
+// Outputs:
+// [ 'undefined', { a: 1, b: 2 } ]
+// [ 'key2', { c: 3, d: 4 } ]
